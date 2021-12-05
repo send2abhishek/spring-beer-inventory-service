b34cc243dee7c2e3efaa5d33b4c6a553af7ef4b7
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-05 14:15:22 +0530

    minor change

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -21,7 +21,7 @@ public class BeerInventoryBootstrap implements CommandLineRunner {
     public static final String BEER_3_UPC = "0083783375213";
     public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
     public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+    public static final UUID BEER_3_UUID = UUID.fromString("6f503693-1aef-4c69-bbe0-c43805396f5e");
 
     private final BeerInventoryRepository beerInventoryRepository;
 
