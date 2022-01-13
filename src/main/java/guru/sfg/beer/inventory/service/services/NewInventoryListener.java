c0f749ae69fb2beb08af2044fb5c8e8a8504fdb4
Abhishek Kumar <abhishek160.kumar@ril.com>
2022-01-13 14:51:23 +0530

    feat: Add queue listener

--- src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -1,6 +1,7 @@
 package guru.sfg.beer.inventory.service.services;
 
 import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
 import guru.sfg.brewery.model.events.NewInventoryEvent;
 import lombok.RequiredArgsConstructor;
@@ -15,18 +16,18 @@ public class NewInventoryListener {
 
     private final BeerInventoryRepository beerInventoryRepository;
 
-
+    // listen the NEW_INVENTORY_QUEUE which is triggered by the beerService
     @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
     public void listen(NewInventoryEvent event) {
 
         log.info("Got Inventory" +event);
-//        System.out.println("got the inventory "+event);
-//        beerInventoryRepository.save(BeerInventory
-//                .builder()
-//                .beerId(event.getBeerDto().getId())
-//                .upc(event.getBeerDto().getUpc())
-//                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
-//                .build());
+        System.out.println("got the inventory "+event);
+        beerInventoryRepository.save(BeerInventory
+                .builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
 
     }
 }
--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-01-12T15:52:20+0530",
+    date = "2022-01-13T12:16:05+0530",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.13 (Oracle Corporation)"
 )
 @Component
